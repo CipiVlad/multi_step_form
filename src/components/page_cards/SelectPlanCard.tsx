dd1d91f54d27d075523e8a0417cacb1986813511
CipiVlad <supercoder.cipi@gmail.com>
2024-03-19 14:22:16 +0100

    select plan logic and styles work

--- src/components/page_cards/SelectPlanCard.tsx
+++ src/components/page_cards/SelectPlanCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import './SelectPlanCard.scss'
 
@@ -13,15 +13,34 @@ type Props = {
     planYearly: any
     icons: any
 }
+
 const SelectPlanCard = ({ title, subtitle, planMonthly, planYearly, monthly, yearly, nextStep, back, icons }: Props) => {
-    const [activeIndex, setActiveIndex] = useState(null);
+    const [activeIndex, setActiveIndex] = useState(null || 0);
+    const [togglePlan, setTogglePlan] = useState(true);
+
+    const [chosenPlan, setChosenPlan] = useState({})
 
     const toggleActiveClass = (index) => {
         setActiveIndex(index === activeIndex ? null : index);
-        console.log(activeIndex)
     };
 
+    const handleTogglePlan = () => {
+        setTogglePlan(!togglePlan)
+    }
 
+    useEffect(() => {
+        //write a function to get the selected plan
+
+        if (!togglePlan) {
+            setChosenPlan(planYearly[activeIndex])
+        } else {
+            setChosenPlan(planMonthly[activeIndex])
+        }
+
+
+    }, [activeIndex, togglePlan]);
+
+    console.log(chosenPlan);
 
     return (
         <div className="stepcard_container">
@@ -29,7 +48,11 @@ const SelectPlanCard = ({ title, subtitle, planMonthly, planYearly, monthly, yea
             <h2>{subtitle}</h2>
 
             <div className='plan_container'>
-                {
+
+                {/* if togglePlan is true display  Monthly Plan  else display Yearly Plan*/}
+
+                {togglePlan ? (
+
                     planMonthly.map((plan, index) => (
                         <div
                             className={`plan_card${index === activeIndex ? ' active' : ''}`}
@@ -45,9 +68,9 @@ const SelectPlanCard = ({ title, subtitle, planMonthly, planYearly, monthly, yea
                             </div>
                         </div>
                     ))
-                }
 
-                {
+                ) : (
+
                     planYearly.map((plan, index) => (
                         <div
                             className={`plan_card${index === activeIndex ? ' active' : ''}`}
@@ -63,14 +86,24 @@ const SelectPlanCard = ({ title, subtitle, planMonthly, planYearly, monthly, yea
                             </div>
                         </div>
                     ))
-                }
+                )}
+
+
             </div>
+
+            {/* test the chosen plan */}
+            <div style={{ textAlign: 'center', fontSize: "16px", border: "1px solid black", padding: "10px", margin: "10px" }} >
+                <span>chosen plan: {chosenPlan.title}</span>
+                <span>price:  {chosenPlan.price}</span>
+                <span>description {chosenPlan.description}</span>
+            </div>
+
 
             <div className="plan_toggle">
                 <p>{monthly}</p>
 
                 <label className="switch">
-                    <input type="checkbox" />
+                    <input type="checkbox" onClick={handleTogglePlan} />
                     <span className="slider round"></span>
                 </label>
                 <p>{yearly}</p>
