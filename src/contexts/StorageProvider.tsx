78ef4fdcf2913bec818f0b8f7873ea3da81a5a63
CipiVlad <supercoder.cipi@gmail.com>
2024-03-23 20:17:54 +0100

    context/storageprovider

---
+++ src/contexts/StorageProvider.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { useContext } from "react"
+
+const initialState: [{}] = [{ name: "", email: "", phone: "" }];
+export const StorageContext = React.createContext(initialState)
+
+const StorageProvider = ({ children }: { children: React.ReactNode }) => {
+
+    const [storage, setStorage] = useState(initialState)
+
+
+    //localStorage set and get
+    useEffect(() => {
+        setStorage(JSON.parse(localStorage.getItem("storage") || "[]"))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem("storage", JSON.stringify(storage))
+    }, [storage])
+
+    const addItem = (data: any) => {
+        setStorage([...storage, data])
+    }
+
+    return (
+        <StorageContext.Provider value={initialState}>
+            {children}
+        </StorageContext.Provider>
+    )
+}
+
+
+export default StorageProvider
