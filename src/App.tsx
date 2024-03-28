8db8828de40de3b56ee5c8a93ff5187052d60e57
CipiVlad <supercoder.cipi@gmail.com>
2024-03-28 23:53:48 +0100

    checklist.txt toDos: added feature for changing css props with states in App.tsx, added plan_card_description for desktop mode <p>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import './index.scss'
 import PersonalInfo from './pages/PersonalInfo'
 import './components/SideBar.scss'
@@ -7,12 +7,44 @@ import AddOns from './pages/AddOns'
 import Finish from './pages/Finish'
 import ThankYou from './pages/ThankYou'
 import StorageProvider from './contexts/StorageProvider'
+import { useEffect, useState } from 'react'
+
+
 
 function App() {
+
+  const style = {
+
+    // Adding media query..
+    '@media (maxWidth: 375px)': {
+      height: '100vh',
+    },
+  };
+
+  const { pathname } = useLocation()
+  console.log(pathname);
+
+  const [addProps, setAddProps] = useState(false)
+  const [height, setHeight] = useState(style)
+
+  //helper function
+
+  useEffect(() => {
+    if (pathname === "/thank-you") {
+      setAddProps(true)
+    } else {
+      setAddProps(false)
+    }
+  }, [pathname])
+
+
   return (
     <StorageProvider>
       <div className="App">
-        <div className="frame_container">
+        <div className="frame_container"
+          //if thank you page is active then set height to 100vh but only for mobile
+          style={pathname === "/thank-you" && window.innerWidth < 768 ? { height: '100vh' } : height}
+        >
           <div className="main_container">
             <Routes>
               <Route path="/" element={<PersonalInfo />} />
@@ -24,7 +56,7 @@ function App() {
           </div>
         </div>
       </div>
-    </StorageProvider>
+    </StorageProvider >
   )
 }
 
--- src/components/page_cards/ThankYouCard.tsx
+++ src/components/page_cards/ThankYouCard.tsx
@@ -2,9 +2,10 @@ import './ThankYouCard.scss'
 import image from '../../assets/images/icon-thank-you.svg'
 import { thankyou } from '../../data/thankyou.module'
 
+
 const ThankYouCard = () => {
     return (
-        <div className="stepcard_container media_mobile">
+        <div className="stepcard_container media_mobile ">
             <div className="thankyou_container">
                 <img src={image} alt={thankyou.imageAlt} />
                 <h1>{thankyou.title}</h1>
